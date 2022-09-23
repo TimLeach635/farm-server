34d08d09c1863e42932a0b92f410bb023c44e460
Tim Leach <timleach635@gmail.com>
2022-09-23 10:40:23 +0100

    Remove debug messages

--- src/server.ts
+++ src/server.ts
@@ -9,15 +9,12 @@ const io = new Server(1995, {
 
 const world = initialise(
   (farmId, cropId) => {
-    console.debug(`Harvesting farm ${farmId} containing crop ${cropId}`);
-
     io.emit("harvest", { farmId, cropId });
   }
 );
 
 world.subscribe((worldState) => {
   io.emit("world update", worldState);
-  console.debug("Sending world update:\n", JSON.stringify(worldState, null, 2));
 });
 
 io.on("connection", () => {
